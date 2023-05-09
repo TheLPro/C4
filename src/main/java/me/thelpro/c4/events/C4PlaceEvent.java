32c44e8c4090c47ffdd8d37aaf46df78fc1c17a1
Leon Potocki <leon.potocki@gmail.com>
2023-05-09 16:39:30 +0200

    test 2

---
+++ src/main/java/me/thelpro/c4/events/C4PlaceEvent.java
@@ -0,0 +1,43 @@
+package me.thelpro.c4.events;
+
+import me.thelpro.c4.C4;
+import me.thelpro.c4.utils.Items;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class C4PlaceEvent implements Listener {
+
+    static C4 plugin = C4.plugin;
+    static FileConfiguration config = plugin.getConfig();
+
+    @EventHandler
+    public void placeEvent(PlayerInteractEvent e) {
+
+        Player player = e.getPlayer();
+        String uuid = player.getUniqueId().toString();
+
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (e.getItem() != null && e.getItem().equals(Items.getC4())) {
+                player.sendMessage("You placed down a C4, use the remote to ignite it.");
+                config.set(uuid, e.getClickedBlock().getLocation());
+            }
+        } else if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (e.getItem() != null && e.getItem().equals(Items.getRemote())) {
+                if (config.contains(uuid)) {
+                    Location loc = config.getLocation(uuid);
+                    player.sendMessage(ChatColor.RED + "BOOM!");
+                    loc.getWorld().spawnEntity(loc, EntityType.PRIMED_TNT);
+                } else {
+                    player.sendMessage(ChatColor.RED + "You need to place a C4 first!");
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/me/thelpro/c4/utils/Items.java
@@ -0,0 +1,47 @@
+package me.thelpro.c4.utils;
+
+import me.thelpro.c4.C4;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+
+public class Items {
+
+    static C4 plugin = C4.plugin;
+    static FileConfiguration config = plugin.getConfig();
+
+
+    public static ItemStack getC4() {
+
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&#99714d&lC4&r, a dangerous explosive.\nCombine it with a remote and it will\n&4destroy&ryour enemies!"));
+
+        ItemStack c4 = new ItemStack(Material.POLISHED_BLACKSTONE_BUTTON);
+        ItemMeta meta = c4.getItemMeta();
+
+        assert meta != null;
+        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', config.getString("item-title")));
+        meta.setLore(lore);
+
+        return c4;
+    }
+
+    public static ItemStack getRemote() {
+
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add(ChatColor.translateAlternateColorCodes('&', "Use this to ignite&#99714d&lC4&r."));
+
+        ItemStack remote = new ItemStack(Material.REDSTONE_TORCH);
+        ItemMeta meta = remote.getItemMeta();
+
+        assert meta != null;
+        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', config.getString("remote-title")));
+        meta.setLore(lore);
+
+        return remote;
+    }
+}
